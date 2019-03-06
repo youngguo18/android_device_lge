2369fdc3f88368d42d38d1451b383c7d52d79081
Ajit Vaishya <ajitv@codeaurora.org>
2019-03-06 19:34:20 +0530

    sm8150-common:wifi: Default compile of WPA based module

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -491,10 +491,17 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
     libwpa_client \
+    libwifi-hal \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    wifi_cli \
+    libwifi-system-iface \
+    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    wifi-mac-generator \
     WifiOverlay.lge_sm8150 \
+    wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf
 
