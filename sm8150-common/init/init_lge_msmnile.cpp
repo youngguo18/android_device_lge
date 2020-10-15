0706122a649047b5d356a123aec8db1c91b0b6c3
Omkar Chandorkar <forumomkar@gmail.com>
2020-10-15 08:40:27 +0530

    sm8150-common:init: handle dalvik props based on sysram

--- sm8150-common/init/init_lge_msmnile.cpp
+++ sm8150-common/init/init_lge_msmnile.cpp
@@ -1,7 +1,5 @@
 /*
-   Copyright (C) 2007, The Android Open Source Project
-   Copyright (c) 2016, The CyanogenMod Project
-   Copyright (c) 2017, The LineageOS Project
+   Copyright (c) 2020, The LineageOS Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -36,6 +34,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/sysinfo.h>
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
@@ -128,7 +127,37 @@ void init_target_properties()
     property_override("ro.product.vendor.model", model);
 }
 
+void load_dalvikvm_properties() {
+  struct sysinfo sys;
+  sysinfo(&sys);
+  if (sys.totalram > 8192ull * 1024 * 1024) {
+    // from - phone-xhdpi-12288-dalvik-heap.mk
+    property_override("dalvik.vm.heapstartsize", "24m");
+    property_override("dalvik.vm.heapgrowthlimit", "384m");
+    property_override("dalvik.vm.heaptargetutilization", "0.42");
+    property_override("dalvik.vm.heapmaxfree", "56m");
+    }
+  else if(sys.totalram > 6144ull * 1024 * 1024) {
+    // from - phone-xhdpi-8192-dalvik-heap.mk
+    property_override("dalvik.vm.heapstartsize", "24m");
+    property_override("dalvik.vm.heapgrowthlimit", "256m");
+    property_override("dalvik.vm.heaptargetutilization", "0.46");
+    property_override("dalvik.vm.heapmaxfree", "48m");
+    }
+  else {
+    // from - phone-xhdpi-6144-dalvik-heap.mk
+    property_override("dalvik.vm.heapstartsize", "16m");
+    property_override("dalvik.vm.heapgrowthlimit", "256m");
+    property_override("dalvik.vm.heaptargetutilization", "0.5");
+    property_override("dalvik.vm.heapmaxfree", "32m");
+  }
+  property_override("dalvik.vm.heapsize", "512m");
+  property_override("dalvik.vm.heapminfree", "8m");
+}
+
 void vendor_load_properties() {
+  // dalvikvm props
+  load_dalvikvm_properties();
     LOG(INFO) << "Loading vendor specific properties";
     init_target_properties();
 }
