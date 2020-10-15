a0b54dcbee405bdcfa7b84ec3cbf3155c4f381be
SGCMarkus <markusornik@gmail.com>
2020-10-15 19:16:12 +0200

    flashlmdd:Drop android.hardware.thermal HAL

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -442,12 +442,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msmnile
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
