9806c50cce68b3e77fd7d11dc57deb27215a3a15
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-11-30 23:51:43 +0300

    sm8150-common: add a rro to disable telephony injection
    doesn't work, doesn't break anything, why care?
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -449,7 +449,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext
+    telephony-ext \
+    TelephonyReslge
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
