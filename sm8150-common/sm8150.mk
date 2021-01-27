6843d825cd23b1e33855a23ba95ea49836c7eb2c
youngguo18 <guoamao@163.com>
2021-01-27 13:48:40 +0800

    sm8150-common:change default cam to CameraGo

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -161,7 +161,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # CNE
@@ -506,3 +505,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+#Prebuilt Apps
+PRODUCT_PACKAGES += \
+    CameraGo
