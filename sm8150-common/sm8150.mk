b12329ccbc0b5ff1afebc18e9fd5a35cc3a9558e
baalajimaestro <me@baalajimaestro.me>
2021-02-21 10:43:14 +0530

    sm8150-common: Switch certain dexopt profiles to verify
    Prebuilt apps like Gmail/Google App, will be updated by google play, and there is no need spend time and space optimising what is going to be replaced.
    Switch to Google's recommendation of using verify for an OTA package.
    Applications still stay on speed.
    Instead, replace the install profile with speed-profile making apps perform better.
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -202,7 +202,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
 # Dex
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
 # DRM
 PRODUCT_PACKAGES += \
