499bd45805ae9e17b5e55dafc92f500474681fe3
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-04-02 11:05:59 +0530

    sm8150-common: uprev vendor.qti.hardware.wifi*
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -490,8 +490,10 @@ PRODUCT_PACKAGES += \
     libwifi-system-iface \
     vendor.qti.hardware.wifi.hostapd@1.0.vendor \
     vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.0.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.2.vendor \
     wifi-mac-generator \
     TetheringConfigOverlay.lge_sm8150 \
     WifiOverlay.lge_sm8150 \
