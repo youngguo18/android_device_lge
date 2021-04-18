ed5e77763ece323f75dcc1873aa2321c412360af
youngguo18 <guoamao@163.com>
2021-04-18 16:18:50 +0800

    Revert "sm8150-common:change default cam to CameraGo"
    This reverts commit 6843d825cd23b1e33855a23ba95ea49836c7eb2c.

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -162,6 +162,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
+    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # CNE
@@ -525,7 +526,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
-
-#Prebuilt Apps
-PRODUCT_PACKAGES += \
-    CameraGo
