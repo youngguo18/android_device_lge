f271c19502f0aefc221c47628c6c51ea35d39087
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-10 13:36:52 +0000

    sm8150-common:  Simplify AVB flag logic

--- sm8150-common/BoardConfigCommon.mk
+++ sm8150-common/BoardConfigCommon.mk
@@ -184,8 +184,7 @@ PRODUCT_FULL_TREBLE_OVERRIDE := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 3
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
