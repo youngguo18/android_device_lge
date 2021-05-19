c00a91f15724cc1dbd101744e0a810266b4818e6
mukesh22584 <mks22584@gmail.com>
2021-05-19 15:44:52 +0530

    sm8150-common: Use armv8-2a for arch variant

--- sm8150-common/BoardConfigCommon.mk
+++ sm8150-common/BoardConfigCommon.mk
@@ -21,7 +21,7 @@ COMMON_PATH := device/lge/sm8150-common
 
 # Platform
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
