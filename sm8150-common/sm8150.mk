5def04a411b848838021644b9acb0091d480535e
youngguo18 <guoamao@163.com>
2021-08-11 14:27:39 +0800

    sm8150-common:update audio hals

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -83,12 +83,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio.common@2.0-util \
-    android.hardware.audio.common@6.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.service \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.bluetooth.audio@2.0-impl \
@@ -99,10 +95,13 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     libaudioroute \
     libhdmiedid \
+    libexthwplugin \
     libhfp \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
+    libsndmonitor \
+    libspkrprot \
     libvolumelistener \
     tinymix \
     libtinycompress \
