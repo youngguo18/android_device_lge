e0d98589d30ba18896d0b116e4826d7a4e9cdb0e
yukiscape <garikapatiamartya@gmail.com>
2021-10-09 12:22:36 +0200

    sm8150-common: Add wpa_supplicant_8_lib to cfi path

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -585,3 +585,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(COMMON_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(COMMON_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# Wlan
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
