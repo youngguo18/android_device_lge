8795949231692c40742303bdbacbacc379c53d04
youngguo18 <guoamao@163.com>
2021-10-29 21:48:17 +0800

    sm8150-common:add back BOARD_SUPPORTS_SOUND_TRIGGER := true

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
