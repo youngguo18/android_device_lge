05415d4e15e9e82ee8a74b6c19748fbb4ce745c4
youngguo18 <guoamao@163.com>
2022-01-03 23:10:57 +0800

    sm8150-common:update android.hardware.audio@6.0 to 7.0

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -86,13 +86,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.common@2.0-util \
-    android.hardware.audio.common@6.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.service \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
