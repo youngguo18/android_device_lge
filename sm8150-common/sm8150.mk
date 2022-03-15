fcca3bbd1ac6073c62a2e0de8e35671a1e4c7ea4
youngguo18 <guoamao@163.com>
2022-03-15 20:33:17 +0800

    sm8150-common: build  android.hardware.camera.device@3.5

--- sm8150-common/sm8150.mk
+++ sm8150-common/sm8150.mk
@@ -150,6 +150,22 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.camera.provider@2.4 \
+    android.hardware.camera.provider@2.4.vendor \
+    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.5.vendor \
+    android.hardware.camera.provider@2.6 \
+    android.hardware.camera.provider@2.6.vendor \
+    android.hardware.camera.device@3.2 \
+    android.hardware.camera.device@3.2.vendor \
+    android.hardware.camera.device@3.3 \
+    android.hardware.camera.device@3.3.vendor \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.4.vendor \
+    android.hardware.camera.device@3.5 \
+    android.hardware.camera.device@3.5.vendor \
+    android.hardware.camera.device@3.6 \
+    android.hardware.camera.device@3.6.vendor \
     libxml2 \
     GCamGo \
     vendor.qti.hardware.camera.device@1.0.vendor
